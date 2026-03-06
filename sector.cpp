3a1338455af47831ff29eafadafc9ebab4106350
Morloking <dyubenkosergei@yandex.ru>
2026-03-07 00:36:44 +0300

    Add files via upload

---
+++ sector.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+void counting_function() {
+    static int count{1};
+    std::cout << "Количество вызовов функции counting_function(): " << count << "\n";
+    ++count;
+}
+
+int main(int argc, char** argv)
+{
+    setlocale(LC_ALL, "RUSSIAN");
+    for (int i = 0; i < 15; i++)
+    {
+        counting_function();
+    }
+}
